d8272c9925828d7e6035b788fe70747b2f1ed973
vladickpetrov <minnesota777@mail.ru>
2022-11-30 11:49:40 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -12,20 +12,18 @@ module.exports.getCards = (req, res) => {
     .populate('owner')
     .populate('likes')
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => {
-      if (err.name === 'CastError') return res.status(ERROR_SERVER).send({ message: 'Что-то пошло не так' });
-    });
+    .catch(() => res.status(ERROR_SERVER).send({ message: 'Что-то пошло не так' }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndDelete(req.params.cardId)
-  .then((cardLikes) => {
-    if (cardLikes == null) return res.status(404).send({ message: 'Карточка не найдена'});
-    res.send({ data: cardLikes })
-  })
+    .then((cardLikes) => {
+      if (cardLikes == null) return res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' });
+      return res.send({ data: cardLikes });
+    })
     .catch((err) => {
-      if (err.name === 'CastError') return res.status(400).send({ message: 'Введен некорректные CardId'});
-      res.status(500).send({ message: 'Что-то пошло не так...' });
+      if (err.name === 'CastError') return res.status(ERROR_INCORRECT).send({ message: 'Введен некорректные CardId' });
+      return res.status(ERROR_SERVER).send({ message: 'Что-то пошло не так...' });
     });
 };
 
@@ -35,39 +33,49 @@ module.exports.createCard = (req, res) => {
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.name === 'ValidationError') return res.status(400).send({ message: 'Введены некорректные данные' })
-      res.status(500).send({ message: 'Что-то пошло не так...' })
+      if (err.name === 'ValidationError') return res.status(ERROR_INCORRECT).send({ message: 'Введены некорректные данные' });
+      return res.status(ERROR_SERVER).send({ message: 'Что-то пошло не так...' });
     });
 };
 
 module.exports.likeCard = (req, res) => {
-  Card.findByIdAndUpdate(req.params.cardId,
-  { $addToSet: { likes: req.user._id } },
-  { new: true })
-  .populate('owner')
-  .populate('likes')
-  .then((cardLikes) => {
-    if (cardLikes == null) return res.status(404).send({ message: 'Карточка не найдена'});
-    res.send({ data: cardLikes })
-  })
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .populate('owner')
+    .populate('likes')
+    .then((cardLikes) => {
+      if (cardLikes == null) return res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' });
+      return res.send({ data: cardLikes });
+    })
     .catch((err) => {
-      if ((req.params.cardId.length !== 24) && (err.name === 'CastError')) return res.status(400).send({ message: 'Введен некорректные CardId'});
-      res.status(500).send({ message: err.name })
+      if ((req.params.cardId.length !== 24) && (err.name === 'CastError')) return res.status(ERROR_INCORRECT).send({ message: 'Введен некорректные CardId' });
+      return res.status(ERROR_SERVER).send({ message: err.name });
     });
 };
 
 module.exports.dislikeCard = (req, res) => {
-  Card.findByIdAndUpdate(req.params.cardId,
-  { $pull: { likes: req.user._id } },
-  { new: true })
-  .populate('owner')
-  .populate('likes')
-  .then((cardLikes) => {
-    if (cardLikes == null) return res.status(404).send({ message: 'Карточка не найдена'});
-    res.send({ data: cardLikes })
-  })
-  .catch((err) => {
-    if ((req.params.cardId.length !== 24) && (err.name === 'CastError')) return res.status(400).send({ message: 'Введен некорректные CardId'});
-    res.status(500).send({ message: 'Что-то пошло не так...' })
-  });
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .populate('owner')
+    .populate('likes')
+    .then((cardLikes) => {
+      if (cardLikes == null) return res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' });
+      return res.send({ data: cardLikes });
+    })
+    .catch((err) => {
+      if ((req.params.cardId.length !== 24) && (err.name === 'CastError')) return res.status(ERROR_INCORRECT).send({ message: 'Введен некорректные CardId' });
+      return res.status(ERROR_SERVER).send({ message: 'Что-то пошло не так...' });
+    });
 };
